c6911eb6d61f917cc0cca8e1caf70eaf40cc92e6
Eugen Neufeld <eneufeld@eclipsesource.com>
2019-01-21 11:46:34 +0100

    Update EntryPoints to parse args

--- com.eclipsesource.icat.import.xml/src/com/eclipsesource/icat/importxml/EntryPoint.java
+++ com.eclipsesource.icat.import.xml/src/com/eclipsesource/icat/importxml/EntryPoint.java
@@ -6,14 +6,25 @@ import java.nio.file.Paths;
 
 public class EntryPoint {
 
+	private static final String defaultFolder = "/home/eugen/Downloads/dekraica/pac_resource_repository/resources/Entries/com/dekra/data/";
+	private static final String defaultEcoreFolder = "/home/eugen/Downloads/dekraica/pac_resource_repository/resources/Entries/com/dekra/data/";
+
 	public static void main(String[] args) throws IOException {
+		String argFolder = null;
+		String argEcoreFolder = null;
+		for (int i = 0; i < args.length; i++) {
+			if ("folder".equalsIgnoreCase(args[i])) {
+				argFolder = args[++i];
+			} else if ("ecoreFolder".equalsIgnoreCase(args[i])) {
+				argEcoreFolder = args[++i];
+			}
+
+		}
 		// parameter
-		Path folderPath = Paths.get("/home/eugen/Downloads/dekraica/pac_resource_repository/resources/Entries/com/dekra/data/");
-		Path ecoreFolderPath = Paths.get("/home/eugen/Downloads/dekraica/pac_resource_repository/resources/Entries/com/dekra/data/");
-		
+		Path folderPath = Paths.get(argFolder == null ? defaultFolder : argFolder);
+		Path ecoreFolderPath = Paths.get(argEcoreFolder == null ? defaultEcoreFolder : argEcoreFolder);
+
 		XMLParser.ecoreFromXml(folderPath, ecoreFolderPath);
 	}
-
-	
 
 }
