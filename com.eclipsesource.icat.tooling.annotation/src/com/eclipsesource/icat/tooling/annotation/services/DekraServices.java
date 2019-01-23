56db6a2035f65ca13d97abe701716802aaa66d30
Tobias Ortmayr <tortmayr@eclipsesource.com>
2019-01-23 16:10:38 +0100

    Fix #25 Table representations sometimes don't have columns
    Add missing implementation for "service:getRoot"  used in the .odesign
    description
    
    Signed-off-by: Tobias Ortmayr <tortmayr@eclipsesource.com>

---
+++ com.eclipsesource.icat.tooling.annotation/src/com/eclipsesource/icat/tooling/annotation/services/DekraServices.java
@@ -0,0 +1,16 @@
+package com.eclipsesource.icat.tooling.annotation.services;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.util.EcoreUtil;
+
+public class DekraServices {
+	/**
+	 * Returns the root container; it may be this object itself
+	 * 
+	 * @param eObject the object to get the root container for.
+	 * @return the root container.
+	 */
+	public EObject getRootContainer(EObject eObject) {
+		return EcoreUtil.getRootContainer(eObject);
+	}
+}
